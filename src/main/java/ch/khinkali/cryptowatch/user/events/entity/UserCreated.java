73c6c2d3d114e99903839306e88495ddd547fa48
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-24 23:03:29 +0100

    [REFAC] use core-events library

--- src/main/java/ch/khinkali/cryptowatch/user/events/entity/UserCreated.java
+++ src/main/java/ch/khinkali/cryptowatch/user/events/entity/UserCreated.java
@@ -3,10 +3,11 @@ package ch.khinkali.cryptowatch.user.events.entity;
 import ch.khinkali.cryptowatch.events.entity.BaseEvent;
 import lombok.Getter;
 
+import javax.json.Json;
 import javax.json.JsonObject;
 
 @Getter
-public class UserCreated extends BaseEvent {
+public class UserCreated implements BaseEvent {
     public static final String TOPIC = "users";
 
     private final String userId;
@@ -21,5 +22,14 @@ public class UserCreated extends BaseEvent {
         this(jsonObject.getString("userId"),
                 jsonObject.getString("username"));
     }
+
+    @Override
+    public JsonObject getJson() {
+        return Json.createObjectBuilder()
+                .add("userId", userId)
+                .add("username", username)
+                .build();
+    }
+
 
 }
