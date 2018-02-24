4fcf6bef7696e5ee1447de0020b08948aff92697
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-24 23:28:47 +0100

    [REFAC] use lombock

--- src/main/java/ch/khinkali/cryptowatch/user/events/entity/UserCreated.java
+++ src/main/java/ch/khinkali/cryptowatch/user/events/entity/UserCreated.java
@@ -1,22 +1,19 @@
 package ch.khinkali.cryptowatch.user.events.entity;
 
 import ch.khinkali.cryptowatch.events.entity.BaseEvent;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 
+@AllArgsConstructor
 @Getter
 public class UserCreated implements BaseEvent {
     public static final String TOPIC = "users";
 
     private final String userId;
     private final String username;
-
-    public UserCreated(final String userId, final String username) {
-        this.userId = userId;
-        this.username = username;
-    }
 
     public UserCreated(JsonObject jsonObject) {
         this(jsonObject.getString("userId"),
