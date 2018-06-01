82c03f50b70c9a1b6a903d9b6f6a7e0621538575
Rob <brem_robert@hotmail.com>
2018-06-01 23:28:39 +0200

    [REFAC] use new core events

--- src/main/java/ch/khinkali/cryptowatch/user/events/entity/UserCreated.java
+++ src/main/java/ch/khinkali/cryptowatch/user/events/entity/UserCreated.java
@@ -1,15 +1,12 @@
 package ch.khinkali.cryptowatch.user.events.entity;
 
 import ch.khinkali.cryptowatch.events.entity.BaseEvent;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.json.Json;
 import javax.json.JsonObject;
 
-@AllArgsConstructor
 @Getter
-public class UserCreated implements BaseEvent {
+public class UserCreated extends BaseEvent {
     public static final String TOPIC = "users";
 
     public enum JSON_KEYS {
@@ -27,13 +24,14 @@ public class UserCreated implements BaseEvent {
     private final String username;
 
     public UserCreated(JsonObject jsonObject) {
-        this(jsonObject.getString(JSON_KEYS.USER_ID.getJsonKey()),
-                jsonObject.getString(JSON_KEYS.USERNAME.getJsonKey()));
+        super(jsonObject.getString(BaseEvent.JSON_KEYS.ID.getJsonKey()),
+                jsonObject.getJsonNumber(BaseEvent.JSON_KEYS.TIMESTAMP.getJsonKey()).longValue());
+        userId = jsonObject.getString(JSON_KEYS.USER_ID.getJsonKey());
+        username = jsonObject.getString(JSON_KEYS.USERNAME.getJsonKey());
     }
 
-    @Override
     public JsonObject getJson() {
-        return Json.createObjectBuilder()
+        return getJsonBuilder()
                 .add(JSON_KEYS.USER_ID.getJsonKey(), userId)
                 .add(JSON_KEYS.USERNAME.getJsonKey(), username)
                 .build();
